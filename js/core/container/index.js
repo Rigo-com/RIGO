f2c5b0c1bc9ad3850aca43b4ec99ccebc920a8a1
Rigo-com <azizo436618@gmail.com>
2026-06-10 02:31:08 +0300

    Update index.js

--- js/core/container/index.js
+++ js/core/container/index.js
@@ -1,1 +1,14 @@
+// =====================================
+// RIGO AI
+// CONTAINER INDEX
+// =====================================
 
+export {
+  Container
+}
+from "./container.js";
+
+export {
+  default
+}
+from "./container.js";
