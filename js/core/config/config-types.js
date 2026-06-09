481875a95fa52bfae1e4872f90a886fe983b26f2
Rigo-com <azizo436618@gmail.com>
2026-06-10 02:42:28 +0300

    Update config-types.js

--- js/core/config/config-types.js
+++ js/core/config/config-types.js
@@ -1,1 +1,50 @@
+// =====================================
+// RIGO AI
+// CONFIG TYPES
+// =====================================
 
+export const CONFIG_SOURCES =
+Object.freeze({
+
+  DEFAULT:
+  "default",
+
+  LOCAL:
+  "local",
+
+  REMOTE:
+  "remote",
+
+  RUNTIME:
+  "runtime"
+
+});
+
+
+
+export const CONFIG_STATES =
+Object.freeze({
+
+  UNLOADED:
+  "unloaded",
+
+  LOADED:
+  "loaded",
+
+  FAILED:
+  "failed"
+
+});
+
+
+
+export default
+Object.freeze({
+
+  SOURCES:
+  CONFIG_SOURCES,
+
+  STATES:
+  CONFIG_STATES
+
+});
